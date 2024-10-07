6d1e45829cfecc81869b1fe255f40be07ef36618
qdelamea <qdelamea@aneo.fr>
2024-10-07 22:14:47 +0200

    feat: add mypy type checking

--- src/armonik_cli/cli.py
+++ src/armonik_cli/cli.py
@@ -6,7 +6,7 @@ from armonik_cli import commands, version
 
 @click.group(name="armonik")
 @click.version_option(version=version.__version__, prog_name="armonik")
-def cli():
+def cli() -> None:
     """
     ArmoniK CLI is a tool to monitor and manage ArmoniK clusters.
     """
